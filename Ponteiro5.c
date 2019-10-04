aee253a1133eba5f88561048a366dcc9956031d5
mltlima <54447895+mltlima@users.noreply.github.com>
2019-10-04 01:25:52 -0300

    Create Ponteiro5.c

---
+++ Ponteiro5.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+
+int  expo(unsigned int base, unsigned int expoente, *)
+{ 
+    unsigned int result;
+    if (expoente != 0)
+    {   
+        printf("%u\n",base);
+        result = ("%u\n",base * expo(base, expoente -1 ));
+        return (base * expo(base, expoente -1 ));
+        
+    }
+    else if (expoente == 0){
+        printf("%u",result);
+       return 1;
+    }
+    else
+        return 0;
+    
+    
+    
+    
+    
+   
+}
+
+int main(void)
+{
+    unsigned int base,expoente,result;
+
+    printf("Digite o valor da base\n");
+    scanf("%u",&base);
+    
+    printf("Digite o valor do expoente\n");
+    scanf("%u",&expoente);
+
+    expo(base,expoente);
+
+
+
+}
+
