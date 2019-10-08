850573ee3919fd9fef09860273fee7f600201e3a
mltlima <54447895+mltlima@users.noreply.github.com>
2019-10-08 20:50:46 -0300

    Create DoublePointer.c

---
+++ DoublePointer.c
@@ -0,0 +1,34 @@
+/*
+ *  Aluno: Miguel Lima Tavares
+ *  Uma função que recebe como argumento um ponteiro
+ *  para um vetor de ponteiros para caracteres
+ *  Area de memoria de 10 elementos
+ *  cada elemento aponta para uma string de tamanho 30.
+ *  UFRJ
+ */
+#include<stdio.h>
+#include<stdlib.h>
+
+int funcao(char **p)
+{   
+   char **p2;
+   printf("%p\n",p);
+   p2 = (char **)malloc(10 * sizeof(char *));
+   p = p2;
+   printf("%p\n",p);
+   for (int  i = 0; i < 10; i++)
+   {
+       *(p + i) = (char *)malloc(30 * sizeof(char));
+       printf("%p\n",*(p + i)); 
+   }
+   
+}
+
+int main(void)
+{
+  char ** p1;
+  funcao(p1);
+   
+   return 0;
+   
+}
