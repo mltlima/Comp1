aafd38b5ff4838c0e63b94d8ca269adce9db67e9
mltlima <54447895+mltlima@users.noreply.github.com>
2019-10-29 21:33:38 -0300

    Update Struct2.c

--- Struct2.c
+++ Struct2.c
@@ -63,6 +63,27 @@ void dados()
             printf("Nome invalido, digite novamente: ");
             gets(agenda[i].nome.sobrenome);
         }
+        
+        printf("Digite o dia de aniversario do usuario %d: ",i + 1);
+        gets(agenda[i].aniversario.dia);
+        while (agenda[i].aniversario.dia < 1 || agenda[i].aniversario.dia > 31){
+            printf("Dia invalido, digite novamente: ");
+            gets(agenda[i].aniversario.dia);
+        }
+
+        printf("Digite o mes de aniversario do usuario %d: ",i + 1);
+        gets(agenda[i].aniversario.mes);
+        while (agenda[i].aniversario.mes < 1 || agenda[i].aniversario.mes > 12){
+            printf("Mes invalido, digite novamente: ");
+            gets(agenda[i].aniversario.mes);
+        }
+
+        printf("Digite o ano de aniversario do usuario %d: ",i + 1);
+        gets(agenda[i].aniversario.ano);
+        while (agenda[i].aniversario.ano < 1 || agenda[i].aniversario.ano > 9999){
+            printf("Ano invalido, digite novamente: ");
+            gets(agenda[i].aniversario.ano);
+        }
 
         printf("Digite o email do usuario %d\n",i + 1);
         scanf("%200s",agenda[i].email);
@@ -129,13 +150,63 @@ void dados()
            gets(agenda[i].telefone.ddd);
         }
 
+        printf("Numero de telefone: ");
+        gets(agenda[i].telefone.numero);
+        while (strlen(agenda[i].telefone.numero) > 11)
+        {
+           printf("Digite um telefone valido: ");
+           gets(agenda[i].telefone.numero);
+        }
+
+        printf("Alguma observacao?\n");
+        gets(agenda[i].observacao);
+
 
     }
 
 }
 
+void nome(void)
+{
+    char first[51];
+    int check = 0; // verifica se algum usuario foi encontrado
+
+    printf("Digite o primeiro nome do usuario que deseja fazer a busca\n");
+    gets(first);
+    while(strlen(first) > 50){
+        first[0] = '\0';
+        printf("Digite um nome valido: ");
+        gets(first);
+    }
+
+    for (int i = 0; i < 100; i++)
+    {
+        if (strcmp(first,agenda[i].nome.primeiroNome) == 0){
+            printf("\nNOME: %s %s\n", agenda[i].nome.primeiroNome,agenda[i].nome.sobrenome);
+            printf("ANIVERSARIO: %i/%i/%i\n", agenda[i].aniversario.dia,agenda[i].aniversario.mes,agenda[i].aniversario.ano);
+            printf("EMAIL: %s\n", agenda[i].email);
+            printf("TELEFONE: %s %s\n", agenda[i].telefone.ddd,agenda[i].telefone.numero);
+            printf("RUA: %s, %i\n", agenda[i].endereco.rua,agenda[i].endereco.numero);
+            printf("COMPLEMENTO: %s\n",agenda[i].endereco.complemento);
+            printf("CEP: %s\n", agenda[i].endereco.CEP);
+            printf("BAIRRO: %s\n", agenda[i].endereco.bairro);
+            printf("CIDADE: %s\n", agenda[i].endereco.cidade);
+            printf("ESTADO: %s\n", agenda[i].endereco.estado);
+            printf("PAIS: %s\n", agenda[i].endereco.pais);
+            printf("OBS: %s\n", agenda[i].observacao);
+            check ++;
+            
+        }
+    }
+    if (check == 0)
+        printf("Nome nao foi encontrada\n");
+}
+
+
 int main(void)
 {
+    dados();
+    nome();
 
     return 0;
 }
